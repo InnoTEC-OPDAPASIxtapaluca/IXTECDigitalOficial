33e7148aead7d63668c8017072c8c2655179c795
InnoTEC-OPDAPASIxtapaluca <innovacion.tecnologica.2024@gmail.com>
2025-07-10 12:15:33 -0600

    Delete IXTEC digital OFICIAL directory

--- IXTEC digital OFICIAL/style.js
+++
@@ -1,7 +0,0 @@
-const textoAnimado = document.querySelector('.texto-animado');
-
-// Ejemplo: Pausar la animación al hacer clic
-textoAnimado.addEventListener('click', () => {
-    textoAnimado.style.animationPlayState = 
-        (textoAnimado.style.animationPlayState === 'paused') ? 'running' : 'paused';
-});
